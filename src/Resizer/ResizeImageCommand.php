71324812fe727fa045b65be7672a3801c0be015c
phakpoom <im_bonbonn@hotmail.com>
2020-01-24 01:46:33 +0700

    Update ResizeImageCommand.php

--- src/Resizer/ResizeImageCommand.php
+++ src/Resizer/ResizeImageCommand.php
@@ -3,12 +3,14 @@
 namespace Resizer;
 
 use Imagine\Filter\Basic\Autorotate;
+use Imagine\Filter\Basic\WebOptimization;
 use Imagine\Image\Box;
 use Imagine\Image\ImageInterface;
 use Imagine\Imagick\Imagine;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
@@ -17,6 +19,8 @@ use Utils\Utils;
 
 class ResizeImageCommand extends Command
 {
+    const FOLDER_NAME = 'resized';
+
     protected static $defaultName = 'resize';
 
     protected function configure()
@@ -24,43 +28,74 @@ class ResizeImageCommand extends Command
         $this
             ->setDescription('Resize multiple images.')
             ->addArgument('lookup', InputArgument::REQUIRED, 'Root dir for lookup')
-            ->addArgument('size', InputArgument::REQUIRED, 'Size in Pixel');
+            ->addArgument('size', InputArgument::OPTIONAL, 'Size in Pixel')
+            ->addOption('quality', 'quality', InputOption::VALUE_OPTIONAL, 'quality', 60);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dir = realpath($input->getArgument('lookup'));
 
-        $images = (new Finder())->in($dir)->files();
+        $images = (new Finder())
+            ->in($dir)
+            ->files()
+            ->name('*.{png,jpg,jpeg,dif}')
+            ->exclude(self::FOLDER_NAME);
 
         $fs = new Filesystem();
         $io = new SymfonyStyle($input, $output);
 
         /** @var \SplFileInfo $image */
-        $size = explode('x', $input->getArgument('size'));
-        $width = $size[0];
-        $height = $size[1] ?? $width;
-        $imagine = new Imagine();
+        if (!empty($input->getArgument('size'))) {
+            $size = explode('x', $input->getArgument('size'));
+            $width = $size[0];
+            $height = $size[1] ?? $width;
+        }
 
-        $folderBase = $dir . '/resized/';
+        $folderBase = $dir . '/' . self::FOLDER_NAME . '/';
 
         if (!$fs->exists($folderBase)) {
             $fs->mkdir($folderBase);
         }
 
+        $save = 0;
         foreach ($images as $image) {
+            $imagine = new Imagine();
+
             $beforeSize = $image->getSize();
-            $imagineImage = $imagine->open($image->getRealPath())
-                ->thumbnail((new Box($width, $height)), ImageInterface::THUMBNAIL_INSET);
+            $imagineImage = $imagine->open($image->getRealPath());
+
+            if (isset($width, $height)) {
+                $imagineImage = $imagineImage->thumbnail((new Box($width, $height)), ImageInterface::THUMBNAIL_INSET);
+            }
 
             $imagineImage = (new Autorotate())->apply($imagineImage);
+            $imagineImage = (new WebOptimization($newFileRealPath = $folderBase . $image->getBasename(), [
+                'quality' => (int) $input->getOption('quality'),
+            ]))->apply($imagineImage);
 
-            $imagineImage->save($newFileRealPath = $folderBase . $image->getBasename());
+            $imagineImage->getImagick()->clear();
 
-            $io->success(sprintf('Resize! %s | %s -> %s',
+            $message = sprintf('Resize! %s | %s -> %s',
                 $image->getFilename(),
                 Utils::formatBytes($beforeSize),
-                Utils::formatBytes(filesize($newFileRealPath))
+                Utils::formatBytes($afterSize = filesize($newFileRealPath))
+            );
+
+            $currentSave = $beforeSize - $afterSize;
+
+            if ($currentSave < 0) {
+                $io->warning($message);
+            } else {
+                $io->success($message);
+            }
+
+            $save += $currentSave;
+        }
+
+        if (0 < $save) {
+            $io->success(sprintf('Save! %s',
+                Utils::formatBytes($save)
             ));
         }
 
